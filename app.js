240d64eab1de9cfb975cadc14118b269ada9ca07
Syed Sulaiman Yousuff (NCS) <syedsulaiman.sulaimanyousuff@ncs.com.sg>
2021-09-21 17:41:27 +0800

    com

--- app.js
+++ app.js
@@ -1,9 +1,48 @@
+const https = require('https');
 const cron = require('node-cron');
 const express = require('express');
 const port = process.env.PORT;
-
+const options = {
+  hostname: 'www.careers.hrp.gov.sg',
+  path: '/sap/opu/odata/sap/ZGERCGS001_SRV/$metadata?saml2=disabled',
+  method: 'GET'
+};
 var app = express();
-cron.schedule('*/5 * * * *', function() {
-    console.log('running a task every 5 mins');
-  });
+cron.schedule('* * * * *', function () {
+  validateCG();
+});
 app.listen(port);
+
+
+function validateCG() {
+  const req = https.request(options, res => {
+    console.log(`statusCode1: ${res.statusMessage}`)
+    if (`${res.statusCode}` != 200) {
+      sendMessage(`${res.statusCode}`, `${res.statusMessage}`);
+    }
+
+  })
+  req.on('error', error => {
+    console.error(error)
+  })
+  req.end()
+}
+
+
+function sendMessage(resStatusCode, resMessage) {
+  console.log("sendMessage invoked");
+  const errorMessage = encodeURIComponent('Metadata loading has failed from internet system with status code ' + resStatusCode + '-' + resMessage),
+    message = {
+      hostname: 'api.telegram.org',
+      path: '/bot2029179703:AAHjjDxykIPDyELQ5aLGoukiIYr1YcimKyI/sendMessage?chat_id=-1001584898445&text=' + errorMessage,
+      method: 'GET'
+    };
+  const req2 = https.request(message, res => {
+    console.log(`statusCode: ${res.statusCode}`);
+  })
+  req2.on('error', error => {
+    console.error(error);
+  })
+  req2.end();
+
+}
